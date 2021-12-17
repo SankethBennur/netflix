fc6d9dc1106f4451189688c55493d2133a0f48d2
SankethBennur <snkthbnnr3@gmail.com>
2021-12-18 01:05:08 +0530

    Admin Application

---
+++ admin/src/pages/home/Home.jsx
@@ -0,0 +1,68 @@
+import { useEffect, useState, useMemo } from "react";
+import Chart from "../../components/chart/Chart";
+import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo";
+import "./home.css";
+import { userData } from "../../dummyData";
+import WidgetSm from "../../components/widgetSm/WidgetSm";
+import WidgetLg from "../../components/widgetLg/WidgetLg";
+import axios from "../../axios";
+
+
+export default function Home() {
+  const MONTHS = useMemo(
+    () => [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Agu",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ],
+    []
+  );
+
+  const [userStats, setUserStats] = useState([]);
+
+  useEffect(() => {
+    const getStats = async () => {
+      try {
+        const res = await axios.get("/user/stats", {
+          headers: {
+            token:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxYmFmZDdlZWI3MjdmYTI1MGFmMDg5MCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTYzOTc2ODA5NywiZXhwIjoxNjQwMjAwMDk3fQ.IRufKT3aIWhduEYa8mXfF9JJRBI3su4WEK_ng9OwF3c",
+          },
+        });
+        const statsList = res.data.sort(function (a, b) {
+          return a._id - b._id;
+        });
+        statsList.map((item) =>
+          setUserStats((prev) => [
+            ...prev,
+            { name: MONTHS[item._id - 1], "New User": item.total },
+          ])
+        );
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getStats();
+    // console.log(userStats);
+  }, [MONTHS]);
+
+  return (
+    <div className="home">
+      <FeaturedInfo />
+      <Chart data={userStats} title="User Analytics" grid dataKey="New User"/>
+      <div className="homeWidgets">
+        <WidgetSm/>
+        <WidgetLg/>
+      </div>
+    </div>
+  );
+}
