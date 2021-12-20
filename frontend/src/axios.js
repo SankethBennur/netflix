9d6b1348739d01862070b650de8280f4dc544097
SankethBennur <snkthbnnr3@gmail.com>
2021-12-20 16:46:41 +0530

    commit

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,8 +1,8 @@
 const axios = require('axios');
 
 const axiosInstance = axios.create({
-     // baseURL: "https://netflix-db.herokuapp.com/"  // should be baseURL
-     baseURL: "http://localhost:5000/"  // should be baseURL
+     baseURL: "https://netflix-db.herokuapp.com/"  // should be baseURL
+     // baseURL: "http://localhost:5000/"  // should be baseURL
 });
 
 module.exports = axiosInstance;
