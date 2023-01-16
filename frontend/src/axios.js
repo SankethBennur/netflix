bda573ebca85cfcc4e41d5082a16476e8a88649a
SankethBennur <snkthbnnr3@gmail.com>
2023-01-16 16:43:23 +0530

    attempting to re-deploy frontend with backend url

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,8 +1,9 @@
 const axios = require("axios");
 
 const axiosInstance = axios.create({
+  baseURL: "https://us-central1-netflix-backend-6b798.cloudfunctions/", // should be baseURL
   //   baseURL: "https://netflix-db.herokuapp.com/", // should be baseURL
-  baseURL: "http://localhost:5000/", // should be baseURL
+  // baseURL: "http://localhost:5000/", // should be baseURL
 });
 
 module.exports = axiosInstance;
