90530b903d082449f0d0f73b72d7712e7e5d7e37
SankethBennur <snkthbnnr3@gmail.com>
2023-01-17 13:42:28 +0530

    base URL - pointing to hosted backend

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,9 +1,7 @@
 const axios = require("axios");
 
 const axiosInstance = axios.create({
-  baseURL: "https://us-central1-netflix-backend-6b798.cloudfunctions/", // should be baseURL
-  //   baseURL: "https://netflix-db.herokuapp.com/", // should be baseURL
-  // baseURL: "http://localhost:5000/", // should be baseURL
+  baseURL: "https://us-central1-netflix-backend-6b798.cloudfunctions.net/app", // should be baseURL
 });
 
 module.exports = axiosInstance;
