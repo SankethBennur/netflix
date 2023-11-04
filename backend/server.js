d27ddee5c8d71a0c916362f0091ae33478394d71
sanketh-bennur-kasmo <sanketh.bennur@kasmo.co>
2023-11-04 23:21:30 +0530

    Debug: Attempting to resolve cors error

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,13 @@ const listRoute = require('./routes/list.js');
 
 app.use(express.json());
 app.use(cors());
+app.use(function(req, res, next) {
+     res.setHeader('Access-Control-Allow-Origin', '*');
+     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+     res.setHeader('Access-Control-Allow-Credentials', true);
+     next();
+ });
 app.use("/auth", authRoute);
 app.use("/user", userRoute);
 app.use("/movie", movieRoute);
